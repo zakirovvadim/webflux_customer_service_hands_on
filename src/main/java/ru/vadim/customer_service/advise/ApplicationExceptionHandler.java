6e008bfc6ba2e4581d0be9ac03856ea03dea167e
vadim <vadim.zakiroff@gmail.com>
2025-01-04 18:42:50 +0500

    Add ApplicationException advice handler

---
+++ src/main/java/ru/vadim/customer_service/advise/ApplicationExceptionHandler.java
@@ -0,0 +1,47 @@
+package ru.vadim.customer_service.advise;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.vadim.customer_service.exceptions.CustomerNotFoundException;
+import ru.vadim.customer_service.exceptions.InsufficientBalanceException;
+import ru.vadim.customer_service.exceptions.InsufficientShareException;
+
+import java.net.URI;
+import java.util.function.Consumer;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ProblemDetail handleException(CustomerNotFoundException ex) {
+        return build(HttpStatus.NOT_FOUND, ex, problem -> {
+            problem.setType(URI.create("http://example.com/problems/customer-not-found"));
+            problem.setTitle("Customer Not Found");
+        });
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ProblemDetail handleException(InsufficientBalanceException ex) {
+        return build(HttpStatus.NOT_FOUND, ex, problem -> {
+            problem.setType(URI.create("http://example.com/problems/insufficient-balance"));
+            problem.setTitle("Insufficient Balance");
+        });
+    }
+
+    @ExceptionHandler(InsufficientShareException.class)
+    public ProblemDetail handleException(InsufficientShareException ex) {
+        return build(HttpStatus.NOT_FOUND, ex, problem -> {
+            problem.setType(URI.create("http://example.com/problems/insufficient-shares"));
+            problem.setTitle("Insufficient Shares");
+        });
+    }
+
+    private ProblemDetail build(HttpStatus status, Exception ex, Consumer<ProblemDetail> consumer) {
+        var problem = ProblemDetail.forStatusAndDetail(status, ex.getMessage());
+        consumer.accept(problem);
+        return problem;
+    }
+}
